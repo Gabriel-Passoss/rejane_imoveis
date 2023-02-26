a2b37b0fb7c8632d6c0f98e56c59910e5ff76e92
Gabriel dos Passos <gabrielp@MacBook-Air-de-Gabriel.local>
2023-02-26 19:42:20 -0300

    Added adjustments to work tailwindcss with postcss and custom color

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{tsx}"],
+  content: ["./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          700: '#084c7d'
+        }
+      }
+    },
   },
   plugins: [],
 }

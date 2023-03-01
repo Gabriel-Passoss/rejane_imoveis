69e5ee94ffc3658be3858c63f44e38c17d8846e9
Gabriel dos Passos <gabrielp@MacBook-Air-de-Gabriel.local>
2023-03-01 17:40:29 -0300

    Adjusted weight and color of WhatsappButton component

--- web/src/components/whatsappButton.tsx
+++ web/src/components/whatsappButton.tsx
@@ -4,7 +4,7 @@ export function WhatsappButton() {
   return (
     <div className="absolute bottom-4 right-4 flex items-center justify-center h-16 w-16 rounded-full bg-green-500 cursor-pointer">
       <a href="https://api.whatsapp.com/send?phone=5547997913799&text=Ol%C3%A1%2C%20gostaria%20de%20falar%20com%20algum%20corretor.">
-        <WhatsappLogo  color='white' size={30} weight='duotone'/>
+        <WhatsappLogo  color='white' size={33} weight='regular'/>
       </a>
     </div>
   )

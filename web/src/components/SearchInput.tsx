2fa3628a5e569ea816008c27b0dec118f094bc31
Gabriel dos Passos <gabrielp@MacBook-Air-de-Gabriel.local>
2023-03-03 23:50:32 -0300

    Fix SearchInput component

--- web/src/components/SearchInput.tsx
+++ web/src/components/SearchInput.tsx
@@ -35,9 +35,9 @@ export function SearchInput() {
     }
   }
   return (
-    <div className="block md:w-auto w-[70%] md:flex items-center fixed z-10">
-      <Menu as="div" className="inline-block text-left w-full">
-      <Menu.Button className="inline-flex w-full md:w-28 items-center justify-center rounded-t-lg md:rounded-l-lg bg-brand-500 px-3 py-3 text-xs md:text-sm font-medium text-white focus:outline-none focus-visible:ring-white focus-visible:ring-opacity-75">
+    <div className="block w-[70%] md:w-full justify-center md:flex items-center fixed z-10">
+      <Menu as="div" className="inline-block w-full md:w-auto text-left">
+      <Menu.Button className="inline-flex w-full md:w-28 items-center justify-center rounded-l-lg md:rounded-l-lg bg-brand-500 px-3 py-3 text-xs md:text-sm font-medium text-white focus:outline-none focus-visible:ring-white focus-visible:ring-opacity-75">
         {typeOfBusiness == 'BUY' ? "COMPRAR" : "ALUGAR"}
         <CaretDown className="ml-2" weight="bold" aria-hidden={true}/>
       </Menu.Button>

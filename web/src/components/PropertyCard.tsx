039143d7804700fb506dece20e2ae0dfb4e71f8d
Gabriel dos Passos <gabrielp@MacBook-Air-de-Gabriel.local>
2023-03-08 13:16:50 -0300

    Added PropertyCard and dynamic listing on Featured area

---
+++ web/src/components/PropertyCard.tsx
@@ -0,0 +1,66 @@
+import { Bed, Car, MapPin, Ruler, Shower } from "phosphor-react";
+import { Pagination } from "swiper";
+import { SwiperSlide, Swiper } from "swiper/react";
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+interface Property {
+  title: String,
+  typeOfBusiness: String,
+  location: String,
+  images: { id: number; url: string; }[]
+  price_sell: Number,
+  price_rent: Number,
+  total_area: Number,
+  private_area: Number,
+  rooms: Number,
+  bathrooms: Number,
+  car: Number
+}
+
+export function PropertyCard(property: Property) {
+  const currencyOptions = { style: 'currency', currency: 'BRL' };
+
+  return (
+    <a className="h-full w-[80vw] md:h-full md:w-[22vw] bg-white rounded-md drop-shadow-lg mb-10" href="">
+      <Swiper modules={[ Pagination ]} slidesPerView={1} loop={true} pagination={{ dynamicBullets: true }}>
+      {property.images.map((image) => {
+        return (
+          <SwiperSlide className="flex justify-center">
+            <img src={image.url.toString()} alt="Foto imóvel" className="md:h-[35vh] h-[25vh] w-full" />
+          </SwiperSlide>
+        )
+      })}
+      </Swiper>
+      <div className="px-5">
+        <h3 className="mt-3 font-medium text-lg md:text-base min-h-[4rem]">{property.title}</h3>
+        <span className="flex justify-start items-center mt-2 mb-3 text-zinc-400 md:text-sm">
+          <MapPin size={20} className="mr-2"/>
+          {property.location}
+        </span>
+        <span className="font-medium text-brand-500 md:text-base">
+          {property.typeOfBusiness == "SELL" ? `VENDA: ${property.price_sell.toLocaleString('pt-BR', currencyOptions)}` : `ALUGUEL: ${property.price_rent.toLocaleString('pt-BR', currencyOptions)}`}
+        </span>
+      </div>
+      <div className="grid grid-cols-2 gap-2 place-items-center mt-4">
+        <span className="flex" title={property.total_area ? "Área Total" : "Área Privativa"}>
+          <Ruler size={24} className="mr-2"/>
+          {property.total_area != 0 ? `${property.total_area}` : `${property.private_area}`}
+        </span>
+        <span className="flex">
+          <Bed size={24} className="mr-2"/>
+          {`${property.rooms}`}
+        </span>
+        <span className="flex">
+          <Shower size={24} className="mr-2"/>
+          {`${property.bathrooms}`}
+        </span>
+        <span className="flex">
+          <Car size={24} className="mr-2"/>
+          {`${property.car}`}
+        </span>
+      </div>
+    </a>
+  )
+}
--- web/src/layout/Featured.tsx
+++ web/src/layout/Featured.tsx
@@ -1,4 +1,7 @@
-import { Bed, Car, House, MapPin, Ruler, Shower } from "phosphor-react";
+import { House } from "phosphor-react";
+
+import properties from "../mock/properties.json"
+import { PropertyCard } from "../components/PropertyCard";
 
 export function Featured() {
   return (
@@ -11,107 +14,24 @@ export function Featured() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-3 w-full px-[7vw] gap-y-14 mt-16 place-items-center">
-        
-        <a className="h-full w-[80vw] md:h-full md:w-[22vw] bg-white rounded-md drop-shadow-lg mb-10" href="">
-          <img className="md:h-[35vh]" src="https://static.useimobia.com.br/1076/imoveis/71581/M/638-1638457762-1.jpg"/>
-          <div className="px-5">
-            <h3 className="mt-3 font-medium text-lg md:text-base">Brava22 |Praia Brava |More com Exclusividade|</h3>
-            <span className="flex justify-start items-center mt-2 mb-3 text-zinc-400 md:text-sm">
-              <MapPin size={20}/>
-              Praia Brava de Itajaí - Itajaí/SC
-            </span>
-            <span className="font-medium text-brand-500 md:text-base">
-              VENDA: R$1.966.336,00
-            </span>
-          </div>
-          <div className="grid grid-cols-2 gap-2 place-items-center mt-4">
-            <span className="flex">
-              <Ruler size={24}/>:
-              174.00m²
-            </span>
-            <span className="flex">
-              <Bed size={24}/>:
-              2
-            </span>
-            <span className="flex">
-              <Shower size={24}/>:
-              2
-            </span>
-            <span className="flex">
-              <Car size={24}/>:
-              2
-            </span>
-          </div>
-        </a>
-
-        <a className="h-full w-[80vw] md:h-full md:w-[22vw] bg-white rounded-md drop-shadow-lg mb-10" href="">
-          <img className="md:h-[35vh]" src="https://static.useimobia.com.br/1076/imoveis/71616/M/673-1643684274-284-20-whatsapp_image_2017-08-26_at_120108_3refeita.jpg"/>
-          <div className="px-5">
-            <h3 className="mt-3 font-medium text-lg md:text-base">
-              Locação Anual - Apartamento 3 suítes, 3 vagas - Pioneiros - Balneário Cambori
-            </h3>
-            <span className="flex justify-start items-center mt-2 mb-3 text-zinc-400 md:text-sm">
-              <MapPin size={20}/>
-              Pioneiros - Balneário Camboriú
-            </span>
-            <span className="font-medium text-brand-500 md:text-base">
-              ALUGUEL: R$7.600,00/mês
-            </span>
-          </div>
-          <div className="grid grid-cols-2 gap-2 place-items-center mt-4">
-            <span className="flex">
-              <Ruler size={24}/>:
-              370.00m²
-            </span>
-            <span className="flex">
-              <Bed size={24}/>:
-              7
-            </span>
-            <span className="flex">
-              <Shower size={24}/>:
-              4
-            </span>
-            <span className="flex">
-              <Car size={24}/>:
-              0
-            </span>
-          </div>
-        </a>
-
-        <a className="h-full w-[80vw] md:h-full md:w-[22vw] bg-white rounded-md drop-shadow-lg mb-10" href="">
-          <img className="md:h-[35vh]" src="https://static.useimobia.com.br/1076/imoveis/71596/M/653-1639510804-1637178574098_4e820a89-c0bc-4f43-99e4-d6e77fd139a3-1024.jpeg"/>
-          <div className="px-5">
-            <h3 className="mt-3 font-medium text-lg md:text-base">
-              Apartamento com 03 Suítes, 126,00 m2 privativo, sacada integrada em Balneário Camboriú, SC
-            </h3>
-            <span className="flex justify-start items-center mt-2 mb-3 text-zinc-400 md:text-sm">
-              <MapPin size={20}/>
-              Centro - Balneário Camboriú
-            </span>
-            <span className="font-medium text-brand-500 md:text-base">
-              VENDA: R$1990.000,00
-            </span>
-          </div>
-          <div className="grid grid-cols-2 gap-2 place-items-center mt-4">
-            <span className="flex">
-              <Ruler size={24}/>:
-              110.00m²
-            </span>
-            <span className="flex">
-              <Bed size={24}/>:
-              3
-            </span>
-            <span className="flex">
-              <Shower size={24}/>:
-              2
-            </span>
-            <span className="flex">
-              <Car size={24}/>:
-              2
-            </span>
-          </div>
-        </a>
-      
+      {properties.map((property) => {
+        return (
+          <PropertyCard 
+            key={property.id}
+            title={property.title}
+            price_sell={property.price_sell}
+            price_rent={property.price_rent}
+            typeOfBusiness={property.typeOfBusiness}
+            location={`${property.street} - ${property.neighborhood} - ${property.city}/${property.state}`}
+            bathrooms={property.characteristics.bathrooms}
+            car={property.characteristics.car}
+            rooms={property.characteristics.rooms}
+            private_area={property.characteristics.private_area}
+            total_area={property.characteristics.total_area}
+            images={property.images}
+          />
+        )
+      })}
       </div>
     </div>
   )

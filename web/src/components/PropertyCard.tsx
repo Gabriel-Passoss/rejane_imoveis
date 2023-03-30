f0bd0ac3e9fe20818adf05b3c3ab726fbe26a486
Gabriel dos Passos <gabrielp@MacBook-Air-de-Gabriel.local>
2023-03-30 18:59:28 -0300

    Adjusted z-index in PropertyCard and added _blank to target of whatsappButton

--- web/src/components/PropertyCard.tsx
+++ web/src/components/PropertyCard.tsx
@@ -31,7 +31,7 @@ export function PropertyCard(property: Property) {
 
   return (
     <Link className="h-full w-[80vw] md:h-full md:w-[22vw] bg-white rounded-md drop-shadow-lg mb-10 md:hover:scale-105 md:duration-150" to={`/imovel/${property.id}`}>
-      <span className="bg-brand-700 p-1 text-white absolute top-2 right-2">Código: {Number(property.id)}</span>
+      <span className="bg-brand-700 p-1 text-white absolute z-10 top-2 right-2 rounded-md text-sm font-medium flex items-center justify-center">Código: {Number(property.id)}</span>
       <Swiper modules={[ Pagination ]} slidesPerView={1} loop={true} pagination={{ dynamicBullets: true }}>
       {property.images.urls.map((image: string, index: number) => {
         return (

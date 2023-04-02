ca6228d4756ebbd941fac2b52c6623c9c45ad020
Gabriel dos Passos <gabrielp@MacBook-Air-de-Gabriel.local>
2023-04-01 22:56:21 -0300

    Made improvements to the property page and made communication with the api

--- web/src/pages/Property/PropertyDetail.tsx
+++ web/src/pages/Property/PropertyDetail.tsx
@@ -6,113 +6,215 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation'
 import { Header } from '../../components/Header/Header'
-import { Bed, Car, CurrencyDollar, MapPin, Ruler, Shower, Star, Toilet } from 'phosphor-react'
+import { Bed, Car, Check, House, Knife, MapPin, Ruler, Shower, StarFour, Tag } from 'phosphor-react'
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+
+type Property = {
+  id: Number,
+  title: String,
+  createdBy: string
+  price_sell: Number,
+  price_rent: Number,
+  price_season: Number,
+  iptu: Number,
+  condominium: Number,
+  typeOfBusiness: String,
+  date: Date,
+  images: Images,
+  characteristics: Characteristics,
+  street: String,
+  neighborhood: String,
+  city: String,
+  state: String,
+  description: String
+}
+
+type Images = {
+  id: Number,
+  urls: [string]
+}
+
+type Characteristics = {
+  id: Number,
+  rooms: Number,
+  car: Number,
+  bathrooms: Number,
+  suites: Number,
+  kitchens: Number,
+  total_area: Number,
+  private_area: Number,
+  property_type: string,
+  infrastructure: String[] 
+}
 
 export function PropertyDetail() {
+  const [property, setProperty] = useState<Property>()
   const { id } = useParams()
+  
+  useEffect(() => {
+    api.get(`/properties/${id}`).then((response) => {
+      setProperty(response.data)
+    })
+  }, [])
 
   return (
     <div className='flex flex-col items-center bg-gray-200'>
       <Header />
-      <h1 className='mt-[14vh] md:mt-[18vh] w-full flex justify-center p-3 bg-black opacity-60 text-white md:text-2xl text-lg font-medium'>DETALHES DO IMÓVEL - CÓD. {id}</h1>
-      <div className='mt-5 w-[70vw]'>
-        <div className='bg-white py-5 px-5 md:px-10 flex flex-col'>
-            <h1 className='font-medium text-lg md:text-xl max-w-[60vw]'>Diferenciado no ILLUMINATI 3 SUÍTES, 2 VAGAS DE GARAGEM EM BALNEÁRIO CAMBORIÚ</h1>
-            <span className='flex items-center mt-4 text-sm md:text-base text-gray-500'>
-              <MapPin className='mr-1'/>
-              Rua 1520, 111 - Centro - Balneário Camboriú/SC
+      
+      <div className='w-full mt-[14vh] md:mt-[18vh]'>
+        <Swiper  modules={[Navigation, Pagination]} spaceBetween={5} slidesPerView={5} loop={true} pagination={{ dynamicBullets: true}} navigation={true} className='flex items-center'>
+          {property?.images.urls.map((url, index) => {
+            return (
+              <SwiperSlide key={index}>
+                <img src={url} alt="Foto do imóvel" className="h-[30vh] md:h-[40vh]" />
+              </SwiperSlide>
+            )
+          })}
+          
+        </Swiper>
+      </div>
+      
+      <div className='mt-10 flex flex-col items-center'>
+        <div>
+          <h1 className='p-2 text-xl md:text-2xl font-medium text-gray-700 border-b-[0.5px] border-gray-400 border-opacity-30'>
+            {property?.title}
+          </h1>
+          <div className='mt-2 p-2'>
+            <span className='p-[3px] rounded-md bg-gray-600 text-white font-medium text-sm mr-2'>
+              {property?.typeOfBusiness === 'SELL' ? 'VENDA' : property?.typeOfBusiness === 'RENT' ? 'ALUGUEL' : 'VENDA E ALUGUEL'}
             </span>
+            <span className='p-[3px] rounded-md bg-gray-600 text-white font-medium text-sm mr-2'>LANÇAMENTO</span>
+          </div>
+        </div>
+        
+        <div className='md:flex items-center justify-between mt-10 p-7 bg-gray-600 rounded-md w-[92vw] md:w-[55vw]'>
+          <h1 className='font-bold text-base md:text-xl text-white'>
+            {
+            property?.typeOfBusiness === 'SELL' ? property.price_sell.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}) : 
+            property?.typeOfBusiness === 'RENT' ? property.price_rent.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}) : 
+            property?.typeOfBusiness === 'SEASON' ? property.price_season.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'}) :
+            `${property?.price_sell.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})} | ${property?.price_rent.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}`
+            }
+          </h1>
+          <div className='flex flex-col md:flex-row mt-1 md:mt-0 md:gap-x-5'>
+            <span className='font-bold text-white text-sm md:text-base'>
+              Condomínio: {property?.condominium.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}
+            </span>
+            <span className='font-bold text-white text-sm md:text-base'>
+              IPTU: {property?.iptu.toLocaleString('pt-br', {style: 'currency', currency: 'BRL'})}
+            </span>
+          </div>
         </div>
 
-        <div className='flex flex-col md:flex-row items-center md:justify-between mt-8'>
-          <div className='w-[90vw] md:w-[40vw]'>
-            <Swiper 
-            modules={[Navigation, Pagination]}
-            slidesPerView={1}
-            loop={true}
-            pagination={{ dynamicBullets: true}}
-            navigation={true}
-            >
-            <SwiperSlide className="flex justify-center">
-              <img src="/slide1.jpg" alt="Slide1" className="h-[30vh] w-[90vw] md:h-[50vh] md:w-[40vw]" />
-            </SwiperSlide>
+        <div className='mt-10 bg-white rounded-md px-4 md:px-10 py-5 w-[90vw] md:w-[55vw]'>
 
-            <SwiperSlide className="flex justify-center">
-              <img src="/slide2.jpg" alt="Slide2" className="h-[30vh] w-[90vw] md:h-[50vh] md:w-[40vw]" />
-            </SwiperSlide>
-            </Swiper>
+          <div className='flex flex-col justify-start'>
+            <span className='font-medium text-base md:text-lg text-gray-600 flex items-center'>
+              <Tag size={20} weight='fill' className='text-brand-700 mr-1'/>
+              {property?.id.toString()}
+            </span>
+
+            <span className='font-medium text-base md:text-lg text-gray-600 flex items-center'>
+              <House size={20} weight='fill' className='text-brand-700 mr-1'/>
+              {property?.characteristics.property_type}
+            </span>
+
+            <span className='font-medium text-base md:text-lg text-gray-600 flex items-center'>
+              <MapPin size={20} weight='fill' className='text-brand-700 mr-1'/>
+              {property?.street}, {property?.neighborhood}, {property?.city} / {property?.state}
+            </span>
           </div>
 
-          <div className='flex flex-col gap-y-5'>
-            <div className='mt-5 md:mt-0 bg-white flex flex-col items-center justify-center p-10 max-h-[40vh]'>
-              <h2 className='mb-6 font-medium text-lg'>DETALHES DO IMÓVEL</h2>
-              <div className='flex flex-col items-center justify-center gap-2'>
-                <span className='font-medium flex items-center'>
-                  <CurrencyDollar className='mr-1 text-brand-500' size={20}/>
-                  Negócio: VENDA
-                </span>
-                <span className='flex items-center'>
-                  <Bed className='mr-2 text-brand-500' size={20}/>
-                  Dormitórios: 3
-                </span>
-                <span className='flex items-center'>
-                  <Shower className='mr-2 text-brand-500' size={20}/>
-                  Banheiros: 3
-                </span>
-                <span className='flex items-center'>
-                  <Toilet className='mr-2 text-brand-500' size={20}/>
-                  Lavabos: 1
-                </span>
-                <span className='flex items-center'>
-                  <Car className='mr-2 text-brand-500' size={20}/>
-                  Garagens: 2
-                </span>
-                <span className='flex items-center'>
-                  <Ruler className='mr-2 text-brand-500' size={20}/>
-                  Área Privativa: 353.00m2
-                </span>
-                <span className='flex items-center'>
-                  <Ruler className='mr-2 text-brand-500' size={20}/>
-                  Área Total: 400.00m2
-                </span>
-              </div>
-              
-            </div>
+          <div className='mt-10 grid grid-cols-2 md:grid-cols-4 gap-y-3 gap-x-10 border-b-[0.5px] py-4'>
+            {Number(property?.characteristics.rooms) > 0 ? 
+              <span className='flex items-center text-sm md:text-base text-gray-500'>
+                <Bed size={15} className='mr-1'/>
+                {property?.characteristics.rooms.toString()} dormitório(s)
+              </span>
+            : null
+            }
+            
+            {Number(property?.characteristics.bathrooms) > 0 ?
+              <span className='flex items-center text-sm md:text-base text-gray-500'>
+              <Shower size={15} className='mr-1'/>
+              {property?.characteristics.bathrooms.toString()} banheiro(s)
+              </span>
+            : null
+            }
+            
+            {Number(property?.characteristics.car) > 0 ?
+              <span className='flex items-center text-sm md:text-base text-gray-500'>
+              <Car size={15} className='mr-1'/>
+                {property?.characteristics.car.toString()} Vaga(s)
+              </span>
+            : null
+            }
 
-            <div className='mt-5 md:mt-0 bg-white flex flex-col items-center justify-center p-10 max-h-[40vh]'>
-              <h1>Características do imóvel</h1>
-              <ul>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>3 pavimentos de lazer</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>2 elevadores</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Sky Lounge no topo do edifício</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Sky bar</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Piscina adulto e infantil</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Academia e pilates</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Sala de jogos</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Cinema</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Sauna</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Spa com hidromassagem</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Beauty Estetic</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Sala de massagem</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Salão de festas com churrasqueira</li>
-                <li className='flex items-center'><Star size={10} className='text-brand-700 mr-1'/>Quiosque com churrasqueira a carvão</li>
+            {Number(property?.characteristics.suites) > 0 ?
+              <span className='flex items-center text-sm md:text-base text-gray-500'>
+              <StarFour size={15} className='mr-1'/>
+                {property?.characteristics.suites.toString()} Suíte(s)
+              </span>
+            : null
+            }
+
+            {Number(property?.characteristics.kitchens) > 0 ?
+              <span className='flex items-center text-sm md:text-base text-gray-500'>
+              <Knife size={15} className='mr-1'/>
+              {property?.characteristics.kitchens.toString()} Cozinha(s)
+              </span>
+            : null
+            }
+
+            {Number(property?.characteristics.total_area) > 0 ?
+              <span className='flex items-center text-sm md:text-base text-gray-500'>
+              <Ruler size={15} className='mr-1'/>
+                {property?.characteristics.total_area.toString()}m² Área total
+              </span>
+            : null
+            }
+
+            {Number(property?.characteristics.private_area) > 0 ?
+              <span className='flex items-center text-sm md:text-base text-gray-500'>
+              <Ruler size={15} className='mr-1'/>
+              {property?.characteristics.private_area.toString()}m² Área privativa
+              </span>
+            : null
+            }
+
+          </div>
+
+          <div className='flex flex-col items-center border-b-[0.5px] py-4'>
+            <h3 className='text-xl font-medium'>Características</h3>
+
+            <div className='mt-5'>
+              <ul className='grid grid-cols-3 md:grid-cols-5 gap-3 justify-center items-center whitespace-nowrap'>
+                {property?.characteristics.infrastructure.map((characteristic, index) => {
+                  return (
+                    <li className='font-medium flex items-center justify-center' key={index}>
+                      <Check size={20} className='text-brand-500 mr-1'/>
+                      {characteristic}
+                    </li>
+                  )
+                })}
+                
               </ul>
             </div>
           </div>
-          
-        </div>
-        <div>
-          <span>
-            Venha viver em Balneário Camboriú com todo conforto e sofisticação que sua família merece.
-            O edifício
 
-            Consulte condições de pagamento facilitado.
-            Entrega prevista para 2020
-            **Fotos ilustrativas
-          </span>
+          <div className='mt-10 flex flex-col items-center'>
+            <h2 className='text-xl font-medium'>Descrição do imóvel</h2>
+
+            <div className='mt-10'>
+              <span className=''>
+               {property?.description}
+              </span>
+            </div>
+          </div>
         </div>
+       
       </div>
-      
       
     </div>
   )
